f4a556f9ff86bb166e8b50d9c295378a52b0361d
DimitarTamahkyarov <dimitar.tamahkyarov91@gmail.com>
2022-11-08 22:57:22 +0200

    Add Color and optimization

--- rock_cissors_paper_game.py
+++ rock_cissors_paper_game.py
@@ -1,14 +1,17 @@
 import random
+from colorama import Fore
 
-computers_points = 0
 user_poits = 0
+computers_points = 0
 
+print()
 print('This is the game "Rock, Scissors, Paper". Enjoy!')
 user_name = input("What is your name? ")
 print(f"OK, {user_name}. The game is up to 3 wins.")
+
 while True:
     print()
-    user_choice = input("Choose [r] for Rock, [s] for Scissors, [p] for Paper: ").lower()
+    user_choice = input(Fore.RESET + "Choose [r] for Rock, [s] for Scissors, [p] for Paper: ").lower()
     if user_choice == "r":
         user_choice = "Rock"
     elif user_choice == "s":
@@ -18,36 +21,31 @@ while True:
 
     computer_choice = random.choice(["Rock", "Paper", "Scissors"])
 
-    print(f"{user_name} -> {user_choice} : {computer_choice} <- Computer")
-    print()
+    print(f"({user_name}) {user_choice} : {computer_choice} (Computer)")
 
-    if user_choice == "Rock":
-        if computer_choice == "Rock":
-            print("Draw!")
-        elif computer_choice == "Paper":
-            print("Computer won a point")
+    if user_choice == computer_choice:
+        print(Fore.LIGHTYELLOW_EX + "It's Draw")
+    elif user_choice == "Rock":
+        if computer_choice == "Paper":
+            print(Fore.RED + "Computer won a point")
             computers_points +=1
         elif computer_choice == "Scissors":
-            print(f"{user_name} won a point")
+            print(Fore.GREEN + f"{user_name} won a point")
             user_poits += 1
     elif user_choice == "Paper":
         if computer_choice == "Rock":
-            print(f"{user_name} won a point")
+            print(Fore.GREEN + f"{user_name} won a point")
             user_poits += 1
-        elif computer_choice == "Paper":
-            print("It's Draw")
         elif computer_choice == "Scissors":
-            print("Computer won a point")
+            print(Fore.RED + "Computer won a point")
             computers_points += 1
     elif user_choice == "Scissors":
         if computer_choice == "Rock":
-            print("Computer won a point")
+            print(Fore.RED + "Computer won a point")
             computers_points += 1
         elif computer_choice == "Paper":
-            print(f"{user_name} won a point")
+            print(Fore.GREEN + f"{user_name} won a point")
             user_poits += 1
-        elif computer_choice == "Scissors":
-            print("It's a Draw")
     else:
         print("Invalid Guess! Try again!")
         print(f"The result is still {user_poits}:{computers_points}")
@@ -56,9 +54,9 @@ while True:
     print(f"({user_name}) {user_poits}:{computers_points} (Computer)")
 
     if computers_points == 3:
-        print("Computer Win")
+        print(Fore.RESET + "Computer Win The Game")
         print()
-        command = input("Do you wonna another game? [yes] or [no]")
+        command = input("Do you wonna another game? [yes] or [no] -> ")
         if command == "yes":
             user_poits = 0
             computers_points = 0
@@ -68,7 +66,7 @@ while True:
             print('I will accept this for "no" ')
             break
     elif user_poits == 3:
-        print("User Win")
+        print(f"{user_name} Win The Game")
         print()
         command = input("Do you wonna another game? [yes] or [no] -> ")
         if command == "yes":
